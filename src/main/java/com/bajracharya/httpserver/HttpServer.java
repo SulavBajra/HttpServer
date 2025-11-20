c3fe5dc2dd7c0930d1e6a4f6f1f38fc36ba88fa8
Sulav Bajracharya <sulavb22@gmail.com>
2025-11-20 17:00:15 +0545

    Added proper logging and applied threads to handle multiple request

--- src/main/java/com/bajracharya/httpserver/HttpServer.java
+++ src/main/java/com/bajracharya/httpserver/HttpServer.java
@@ -1,17 +1,31 @@
 package com.bajracharya.httpserver;
 
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.bajracharya.httpserver.config.Configuration;
 import com.bajracharya.httpserver.config.ConfigurationManager;
+import com.bajracharya.httpserver.core.ServerListenerThread;
 
 //Driver Class for the HTTP server
 public class HttpServer {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(HttpServer.class);
     public static void main(String[] args) {
         
         ConfigurationManager.getInstance().loadConfigurationFile("src/main/resources/http.json");
         Configuration conf = ConfigurationManager.getInstance().getCurrentConfiguration();
-        System.out.println(conf);
+        LOGGER.info("Starting Server....");
+        LOGGER.info("Using Port: "+conf.getPort());
+        LOGGER.info("Using web root: "+conf.getWebroot());
 
-        System.out.println("Using Port: "+conf.getPort());
-        System.out.println("Using web root: "+conf.getWebroot());
+        try {
+            ServerListenerThread serverListenerThread = new ServerListenerThread(conf.getPort(), conf.getWebroot());
+            serverListenerThread.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/main/java/com/bajracharya/httpserver/core/HttpConnectionWorkerThread.java
@@ -0,0 +1,54 @@
+package com.bajracharya.httpserver.core;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HttpConnectionWorkerThread extends Thread {
+    private Socket socket;
+    private final static Logger LOGGER = LoggerFactory.getLogger(HttpConnectionWorkerThread.class);
+
+    public HttpConnectionWorkerThread(Socket socket){
+        this.socket = socket;
+    }
+
+    @Override
+    public void run(){
+        InputStream inputStream = null;
+        OutputStream outputStream = null;
+       try {
+            inputStream = socket.getInputStream();
+            outputStream = socket.getOutputStream();
+            //reading
+
+
+            //writing
+            String html = "<html><head><title>http server </title></head><body><h1>Hello</h1></body></html>";
+            final String CRLF = "\n\r";
+            String response = "HTTP/1.1 200 OK"+CRLF+"Content-Length"+html.getBytes().length + CRLF + CRLF + html +
+                                CRLF + CRLF;
+            outputStream.write(response.getBytes());
+
+            LOGGER.info("Connection Processing finished");
+
+       } catch (IOException e) {
+        LOGGER.error("Problem with communication",e);
+       }finally{
+          if(inputStream != null || outputStream != null || socket != null){
+            try{
+                inputStream.close();
+                outputStream.close();
+                socket.close();
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+          }
+       }
+
+
+    }
+}
